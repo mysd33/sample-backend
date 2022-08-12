c891945fa770e8d1a79a863fe223abea8036767a
mysd33 <mysd.33@gmail.com>
2022-08-12 23:09:53 +0900

    BFF側の修正に伴うBackend側の同一フレームワーク実装例の修正

--- src/main/java/com/example/fw/web/resource/ErrorResponse.java
+++ src/main/java/com/example/fw/web/resource/ErrorResponse.java
@@ -2,15 +2,19 @@ package com.example.fw.web.resource;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse  implements Serializable { 
 	private static final long serialVersionUID = -707495429327768166L;
 
-    private final String code;
-    private final String message;
+    private String code;
+    private String message;
 
 }
