1b547f7d424a2a24f73cf45d2893acfa36a4c579
mysd33 <mysd.33@gmail.com>
2022-09-13 22:38:08 +0900

    集約例外ハンドリング機能の横並び反映

--- src/main/java/com/example/fw/web/advice/GlobalRestControllerAdvice.java
+++ src/main/java/com/example/fw/web/advice/GlobalRestControllerAdvice.java
@@ -1,7 +1,5 @@
 package com.example.fw.web.advice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
