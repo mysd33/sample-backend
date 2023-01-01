74f7479d441d860c6652469bbed43418d625fd49
mysd33 <mysd.33@gmail.com>
2023-01-01 13:48:22 +0900

    RDBアクセスのX-Ray対応

--- src/main/java/com/example/backend/SampleBackendApplication.java
+++ src/main/java/com/example/backend/SampleBackendApplication.java
@@ -3,10 +3,13 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amazonaws.xray.AWSXRay;
+
 @SpringBootApplication
 public class SampleBackendApplication {
 
 	public static void main(String[] args) {
+		AWSXRay.beginSegment("sample-backend");	
 		SpringApplication.run(SampleBackendApplication.class, args);
 	}
 
