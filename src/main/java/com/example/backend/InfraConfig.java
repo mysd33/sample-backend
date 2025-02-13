4d8fde8ef29b59d07dadc5d03a4c164a423ebdcb
mysd33 <mysd.33@gmail.com>
2025-02-13 21:25:21 +0900

    @TransactionalのreadOnly属性により動的にデータソース切り替える機能の追加

--- src/main/java/com/example/backend/InfraConfig.java
+++ src/main/java/com/example/backend/InfraConfig.java
@@ -1,9 +1,11 @@
 package com.example.backend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import com.example.backend.domain.repository.TodoRepository;
 import com.example.backend.infra.repository.TodoRepositoryStub;
+import com.example.fw.common.datasource.config.DynamicRoutingDataSourceConfig;
 
 /**
  * 
@@ -11,12 +13,13 @@ import com.example.backend.infra.repository.TodoRepositoryStub;
  *
  */
 @Configuration
+@Import(DynamicRoutingDataSourceConfig.class)
 public class InfraConfig {
 
-	//MyBatisでRDBアクセスしない場合のスタブ	
-	//@Bean
-	TodoRepository todoRepository() {
-		return new TodoRepositoryStub();
-	}
+    // MyBatisでRDBアクセスしない場合のスタブ
+    // @Bean
+    TodoRepository todoRepository() {
+        return new TodoRepositoryStub();
+    }
 
 }
