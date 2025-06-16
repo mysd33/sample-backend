c2760dce617e9a7a2a6706b4a284da3305253c2d
mysd33 <mysd.33@gmail.com>
2025-06-16 22:58:19 +0900

    PostgreSQLのデータ長を元に戻す

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS todo (
     todo_id VARCHAR(36) PRIMARY KEY,
-    todo_title VARCHAR(60),
+    todo_title VARCHAR(30),
     finished BOOLEAN,
     created_at TIMESTAMP
 );
