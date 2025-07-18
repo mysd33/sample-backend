32c9bd677d0a6dbff06f4505ab91071ada16d1ba
mysd33 <mysd.33@gmail.com>
2025-07-19 00:58:32 +0900

    メッセージID体系の誤り修正

--- src/main/java/com/example/backend/domain/message/MessageIds.java
+++ src/main/java/com/example/backend/domain/message/MessageIds.java
@@ -7,8 +7,8 @@ public class MessageIds {
     private MessageIds() {}
     public static final String W_EX_2001 = "w.ex.2001";
     public static final String W_EX_2002 = "w.ex.2002";
-    public static final String W_EX_2003 = "w.ex.2003";
     public static final String W_EX_5001 = "w.ex.5001";
-    public static final String W_EX_5005 = "w.ex.5005";
+    public static final String W_EX_5002 = "w.ex.5002";
+    public static final String W_EX_5003 = "w.ex.5003";
     public static final String E_EX_9001 = "e.ex.9001";
 }
