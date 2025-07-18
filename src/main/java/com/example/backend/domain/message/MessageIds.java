71929ac8908c159bd662cfaf59886c8c3022654b
mysd33 <mysd.33@gmail.com>
2025-07-18 20:35:37 +0900

    集約例外ハンドリングのDefaultErrorResponseCreatorをフレームワーク側パッケージに移動

--- src/main/java/com/example/backend/domain/message/MessageIds.java
+++ src/main/java/com/example/backend/domain/message/MessageIds.java
@@ -9,9 +9,6 @@ public class MessageIds {
     public static final String W_EX_2002 = "w.ex.2002";
     public static final String W_EX_2003 = "w.ex.2003";
     public static final String W_EX_5001 = "w.ex.5001";
-    public static final String W_EX_5002 = "w.ex.5002";
-    public static final String W_EX_5003 = "w.ex.5003";
-    public static final String W_EX_5004 = "w.ex.5004";
     public static final String W_EX_5005 = "w.ex.5005";
     public static final String E_EX_9001 = "e.ex.9001";
 }
