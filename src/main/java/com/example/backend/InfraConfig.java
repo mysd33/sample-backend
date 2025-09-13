cacc625b130d27569dd6d4528e7ce1b95c15cc09
mysd33 <mysd.33@gmail.com>
2025-09-13 09:41:06 +0900

    ロギング拡張機能の修正

--- src/main/java/com/example/backend/InfraConfig.java
+++ src/main/java/com/example/backend/InfraConfig.java
@@ -1,12 +1,13 @@
 package com.example.backend;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.example.backend.domain.repository.TodoRepository;
 import com.example.backend.infra.repository.TodoRepositoryStub;
 import com.example.fw.common.db.config.DynamicRoutingDataSourceConfig;
-import com.example.fw.common.logging.config.LoggingExtensionConfig;
+import com.example.fw.common.logging.config.LoggingConfigPackage;
 import com.example.fw.common.metrics.config.MetricsConfig;
 
 /**
@@ -15,10 +16,10 @@ import com.example.fw.common.metrics.config.MetricsConfig;
  *
  */
 @Configuration
-//動的ルーティングによるデータソース設定を追加
-//Micrometerのカスタムメトリックス設定を追加
-//Loggingの拡張設定を追加
-@Import({ DynamicRoutingDataSourceConfig.class, MetricsConfig.class, LoggingExtensionConfig.class })
+// Loggingの拡張設定を追加
+@ComponentScan(basePackageClasses = { LoggingConfigPackage.class })
+// 動的ルーティングによるデータソース設定、Micrometerのカスタムメトリックス設定を追加
+@Import({ DynamicRoutingDataSourceConfig.class, MetricsConfig.class })
 public class InfraConfig {
 
     // MyBatisでRDBアクセスしない場合のスタブ
