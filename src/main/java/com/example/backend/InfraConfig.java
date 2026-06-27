a40d36fd6be66bccff319ec62dcda60ea4e6783a
mysd33 <mysd.33@gmail.com>
2026-06-27 21:43:11 +0900

    AWS SDKのリトライ、タイムアウト等の設定対応

--- src/main/java/com/example/backend/InfraConfig.java
+++ src/main/java/com/example/backend/InfraConfig.java
@@ -1,21 +1,18 @@
 package com.example.backend;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
 import com.example.backend.domain.repository.TodoRepository;
 import com.example.backend.infra.repository.TodoRepositoryStub;
 import com.example.fw.common.logging.config.LoggingConfigPackage;
-import com.example.fw.common.metrics.config.MetricsConfig;
-import com.example.fw.common.rdb.config.DynamicRoutingDataSourceConfig;
+import com.example.fw.common.metrics.config.MetricsConfigPackage;
+import com.example.fw.common.rdb.config.RDBConfigPackage;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /// インフラストラクチャ層の設定クラス
 @Configuration
-// Loggingの拡張設定を追加
-@ComponentScan(basePackageClasses = { LoggingConfigPackage.class })
-// 動的ルーティングによるデータソース設定、Micrometerのカスタムメトリックス設定を追加
-@Import({ DynamicRoutingDataSourceConfig.class, MetricsConfig.class })
+// Loggingの拡張設定、RDBアクセスの動的データソースルーティング設定、メトリックス転送機能の設定を追加
+@ComponentScan(basePackageClasses = {LoggingConfigPackage.class,
+    MetricsConfigPackage.class, RDBConfigPackage.class}) //
 public class InfraConfig {
 
     // MyBatisでRDBアクセスしない場合のスタブ
